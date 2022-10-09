bd39b4717535cbd52a6b6456132da2e5cc00f254
Naseem Jamal <naseemjamal@Naseems-MacBook-Pro-2.local>
2022-10-09 14:39:16 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { getElementError } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
